23317558473f80bbbe0c92bf7076b5b224a41da3
jakubfalowski <kubafa99@gmail.com>
2022-09-03 21:32:20 +0200

    feat: better map api

--- apps/scrapping/pages/api/fifa.jsx
+++ apps/scrapping/pages/api/fifa.jsx
@@ -1,13 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Fifa from "../fifa";
 
-export default async function FifaAPI(req: NextApiRequest, res: NextApiResponse) {
+export default async function FifaAPI(req,res) {
     const results = await Fifa();
+
     const interval = setInterval(timeToFetch, 1);
     function timeToFetch() {
         if (results[0].length > 645){
             clearInterval(interval);
-            res.json(results);
+            const json = results[0].map((row, index) => {
+                return results.map((attributes) => {
+                  return attributes[index]
+                })
+              })
+            res.json(json);
         }
     }
 }
