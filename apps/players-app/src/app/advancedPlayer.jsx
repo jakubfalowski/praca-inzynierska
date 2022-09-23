e0fd9a06c33a5940df60a1baa5a4cedc61f0a605
jakubfalowski <kubafa99@gmail.com>
2022-09-24 01:15:54 +0200

    feat: sofascore api data select

--- apps/players-app/src/app/advancedPlayer.jsx
+++ apps/players-app/src/app/advancedPlayer.jsx
@@ -1,16 +1,21 @@
-import {useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { playerTabFunction } from './fetchData';
 import { Table } from '@mantine/core';
 import './style.scss'
 import { LoadingOverlay} from '@mantine/core';
+import FetchSofaScore from './fetchSofascore';
+
+let apiData = new Array(0);
 
 export function AdvancedPlayer(){
 
     const [data, setData] = useState();
+
     let { name } = useParams();
 
     const playerTab = new Array(0);
+    
     
     function setPlayer(){
         if(playerTab[0]){
@@ -22,11 +27,24 @@ export function AdvancedPlayer(){
         
     }
 
+    FetchSofaScore(name).then(async (value)=>{
+        if(value && value.error.message !== "Not Found") {
+            apiData.push(
+                {"percentDribbles": value.statistics.successfulDribblesPercentage, "wasFouled": value.statistics.wasFouled,
+                    "percentAerialDuels" : value.statistics.aerialDuelsWonPercentage, "percentGroundDuels": value.statistics.groundDuelsWonPercentage,
+                    "goals" : value.statistics.goals, "shots": value.statistics.totalShots,
+                    "tackles": value.statistics.tackles, "interceptions": value.statistics.interceptions, "clearances": value.statistics.clearances,
+                    "percentPasses": value.statistics.accuratePassesPercentage, "keyPasses": value.statistics.keyPasses, "bigChancesCreated": value.statistics.bigChancesCreated, "assists": value.statistics.assists,
+                    "rating": value.statistics.rating, "minutes": value.statistics.minutesPlayed
+                })
+        }
+        
+    })
+
     playerTabFunction().then((value) => {
         playerTab.push(value)
-    }).then(setPlayer);
+    }).then(setPlayer)
 
-    
 
     return(
         <div className="ap" style={{
@@ -46,48 +64,54 @@ export function AdvancedPlayer(){
                                 <td className='ratingBox'></td>
                                 <td className='ratingBox'>FIFA</td>
                                 <td className='ratingBox'>Football Manager</td>
+                                <td className='ratingBox'>SofaScore</td>
                             </tr>
                         <tr className='statBox'>
                             <td>Overall</td>
                             <td className={parseInt(data[1]) > parseInt(data[2]) ? 'ratingBox better': parseInt(data[1]) === parseInt(data[2]) ? 'ratingBox same' : 'ratingBox worse'}>{data[1]}</td>
                             <td className={parseInt(data[2]) > parseInt(data[1]) ? 'ratingBox better': parseInt(data[1]) === parseInt(data[2]) ? 'ratingBox same' : 'ratingBox worse'}>{data[2]}</td>
+                            <td className='ratingBox same'>{apiData.length > 0 ? <ul><li>Średnia ocena: {(apiData[0].rating).toFixed(2)}</li></ul> : "-"}</td>
                         </tr>
                         <tr className='statBox'>
                             <td>Szybkość</td>
                             <td className={parseInt(data[3]) > parseInt(data[4]) ? 'ratingBox better': parseInt(data[3]) === parseInt(data[4]) ? 'ratingBox same' : 'ratingBox worse'}>{data[3]}</td>
                             <td className={parseInt(data[4]) > parseInt(data[3]) ? 'ratingBox better': parseInt(data[3]) === parseInt(data[4]) ? 'ratingBox same' : 'ratingBox worse'}>{data[4]}</td>
+                            <td className='ratingBox same'>{apiData.length > 0 ? <ul><li>Procent wygranych pojedynków biegowych: {(apiData[0].percentDribbles).toFixed(2)}</li><li>Ilość faulów na danym zawodniku: {apiData[0].wasFouled}</li></ul> : "-"}</td>
                         </tr>
                         <tr className='statBox'>
                             <td>Fizyczność</td>
                             <td className={parseInt(data[5]) > parseInt(data[6]) ? 'ratingBox better': parseInt(data[5]) === parseInt(data[6]) ? 'ratingBox same' : 'ratingBox worse'}>{data[5]}</td>
                             <td className={parseInt(data[6]) > parseInt(data[5]) ? 'ratingBox better': parseInt(data[5]) === parseInt(data[6]) ? 'ratingBox same' : 'ratingBox worse'}>{data[6]}</td>
+                            <td className='ratingBox same'>{apiData.length > 0 ? <ul><li>Procent wygranych pojedynków powietrznych: {(apiData[0].percentAerialDuels).toFixed(2)}</li><li>Procent wygranych pojedynków z piłką na trawie: {(apiData[0].percentGroundDuels).toFixed(2)}</li></ul> : "-"}</td>
                         </tr>
                         <tr className='statBox'> 
                             <td>Atak</td>
                             <td className={parseInt(data[7]) > parseInt(data[8]) ? 'ratingBox better': parseInt(data[7]) === parseInt(data[8]) ? 'ratingBox same' : 'ratingBox worse'}>{data[7]}</td>
                             <td className={parseInt(data[8]) > parseInt(data[7]) ? 'ratingBox better': parseInt(data[7]) === parseInt(data[8]) ? 'ratingBox same' : 'ratingBox worse'}>{data[8]}</td>
+                            <td className='ratingBox same'>{apiData.length > 0 ? <ul><li>Liczba bramek: {apiData[0].goals}</li><li>Liczba strzałów: {apiData[0].shots}</li></ul> : "-"}</td>
                         </tr>
                         <tr className='statBox'> 
                             <td>Defensywa</td>
                             <td className={parseInt(data[9]) > parseInt(data[10]) ? 'ratingBox better': parseInt(data[9]) === parseInt(data[10]) ? 'ratingBox same' : 'ratingBox worse'}>{data[9]}</td>
                             <td className={parseInt(data[10]) > parseInt(data[9]) ? 'ratingBox better': parseInt(data[9]) === parseInt(data[10]) ? 'ratingBox same' : 'ratingBox worse'}>{data[10]}</td>
+                            <td className='ratingBox same'>{apiData.length > 0 ? <ul><li>Odbiory: {apiData[0].interceptions}</li><li>Wślizgi: {apiData[0].tackles}</li><li>Wybicia: {apiData[0].clearances}</li></ul> : "-"}</td>
                         </tr>
                         <tr className='statBox'>
                             <td>Drybling / Podania</td>
                             <td className={parseInt(data[11]) + parseInt(data[12]) > parseInt(data[13])*2 ? 'ratingBox better': parseInt(data[11]) + parseInt(data[12]) === parseInt(data[13]) ? 'ratingBox same' : 'ratingBox worse'}>{data[11]} / {data[12]}</td>
                             <td className={(parseInt(data[13])*2 > parseInt(data[11]) + parseInt(data[12])) ? 'ratingBox better': (parseInt(data[13])*2  === parseInt(data[11]) + parseInt(data[12])) ? 'ratingBox same': 'ratingBox worse'}>{data[13]}</td>
+                            <td className='ratingBox same'>{apiData.length > 0 ? <ul><li>Asysty: {apiData[0].assists}</li><li>Kluczowe podania: {apiData[0].keyPasses}</li><li>Stworzone niebezpieczne sytuacje: {apiData[0].bigChancesCreated}</li><li>Procent celnych podań: {(apiData[0].percentPasses).toFixed(2)}</li></ul> : "-"}</td>
                         </tr>          
                         <tr className='statBox'>
                             <td>Mentalność</td>
                             <td className='ratingBox same'> - </td>
                             <td className='ratingBox same'>{data[14]}</td>
                         </tr>      
-                        {/*  */}
+                        
                     </tbody>
                 </Table> : <LoadingOverlay visible={true} overlayBlur={2} />
             }
         </div>
     )
-
 }
 export default AdvancedPlayer;
--- apps/players-app/src/app/fetchSofascore.jsx
+++ apps/players-app/src/app/fetchSofascore.jsx
@@ -1,27 +1,36 @@
 import { useEffect } from "react";
 
-let ifFetch = true;
-export function FetchSofaScore(){
+let ifFetch = 0;
 
-    async function fetchData(query) {
-        try{
-            const options = {
-                method: 'GET',
-                headers: {
-                  'X-RapidAPI-Key': '35ffe6e4c5mshe6f63287717ea95p1950a0jsna16c5a14a971',
-                  'X-RapidAPI-Host': 'sofascore.p.rapidapi.com'
-                }
-              };
-              const response = await fetch('https://sofascore.p.rapidapi.com/players/get-statistics?playerId=108579&tournamentId=17&seasonId=37036&typ=overall',options);
-              const data = await response.json();
-              console.log(data)
-        } catch(error){
-          console.log(error)
-        }
+export async function FetchSofaScore(nameOfPlayer){
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '35ffe6e4c5mshe6f63287717ea95p1950a0jsna16c5a14a971',
+      'X-RapidAPI-Host': 'sofascore.p.rapidapi.com'
     }
-    
-    useEffect(() => {
-        fetchData();
-      }, [ifFetch]);
+  };
+
+  async function jeden(){ 
+    const response1 = await fetch(`https://sofascore.p.rapidapi.com/players/search?name=${nameOfPlayer}`,options);
+    const data1 = await response1.json();
+    return data1.players[0].id;
+  }
+
+  
+    const returnData = () => {
+      if(ifFetch === 0){
+        const getData = jeden().then(async (playerId) => {
+          const response2 = await fetch(`https://sofascore.p.rapidapi.com/players/get-statistics?playerId=${playerId}&tournamentId=17&seasonId=37036&typ=overall`,options);
+          const data2 = await response2.json();
+          return data2;
+        })
+        ifFetch++;
+        return getData;
+    }
+  }
+      
+  return returnData()
 }
 export default FetchSofaScore;
